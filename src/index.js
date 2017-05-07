52ee39d36c3121251bbdf15e86f848b12ecfb4da
dgsousa <sousa.danielg@gmail.com>
2017-05-07 05:30:45 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-const express = require("express");
-const multer = require("multer");
-const upload = multer({dest: "./uploads/"});
-const logger = require("morgan");
-const jsonParser = require("body-parser").json;
-const app = express();
-const port = process.env.PORT || 3000;
+// const express = require("express");
+// const multer = require("multer");
+// const upload = multer({dest: "./uploads/"});
+// const logger = require("morgan");
+// const jsonParser = require("body-parser").json;
+// const app = express();
+// const port = process.env.PORT || 3000;
 
 
 
-app.use(logger("dev"));
-app.use(jsonParser());
+// app.use(logger("dev"));
+// app.use(jsonParser());
 
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
 
-app.post("/file", upload.any(), (req, res, next) => {
-	res.status(200);
-	res.json({
-		size: req.files[0].size
-	});
-})
-
-
-app.listen(port, () => {
-	console.log("The server is running on port", port);
-})
+// app.post("/file", upload.any(), (req, res, next) => {
+// 	res.status(200);
+// 	res.json({
+// 		size: req.files[0].size
+// 	});
+// })
 
 
+// app.listen(port, () => {
+// 	console.log("The server is running on port", port);
+// })
+
+const http = require("http");
+const port = process.env.PORT || 300;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {"Content-type": "text/html"});
+	res.write("Hello World");
+	res.end();
+}).listen(port);
